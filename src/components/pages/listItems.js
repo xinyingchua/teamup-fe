3dafbba29a4bc1160164b1022352a28de8424f26
lucasseet <79414028+lucasseet@users.noreply.github.com>
2021-07-17 10:08:36 +0800

    dashboard-icon-fixed

--- src/components/pages/listItems.js
+++ src/components/pages/listItems.js
@@ -4,11 +4,15 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import PaletteIcon from '@material-ui/icons/Palette';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ToggleOnIcon from '@material-ui/icons/ToggleOn';
 
 export const mainListItems = (
   <div >
@@ -21,35 +25,35 @@ export const mainListItems = (
 
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <ShoppingCartIcon />
+        <MonetizationOnIcon />
       </ListItemIcon>
       <ListItemText primary="Budget Planning" />
     </ListItem>
 
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <PeopleIcon />
+        <FormatListNumberedIcon />
       </ListItemIcon>
       <ListItemText primary="To Do" />
     </ListItem>
 
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <BarChartIcon />
+        <PeopleIcon />
       </ListItemIcon>
       <ListItemText primary="Guestlists" />
     </ListItem>
 
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <LayersIcon />
+        <DraftsIcon />
       </ListItemIcon>
       <ListItemText primary="Events Scheduling" />
     </ListItem>
 
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <LayersIcon />
+        <PaletteIcon  />
       </ListItemIcon>
       <ListItemText primary="Design E-invites" />
     </ListItem>
@@ -62,14 +66,14 @@ export const secondaryListItems = (
   <div>
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <AssignmentIcon />
+        <ToggleOnIcon />
       </ListItemIcon>
       <ListItemText primary="Dark Mode" />
     </ListItem>
    
     <ListItem button>
       <ListItemIcon style={{ color: 'white' }}>
-        <AssignmentIcon />
+        <ExitToAppIcon />
       </ListItemIcon>
       <ListItemText primary="Log Out" />
     </ListItem>
