3a16a4cfa39a9fa02911a136976a5823f9322c25
Howard Lee <howardlee@Howards-MacBook-Pro.local>
2021-07-25 05:47:51 +0800

    fixed login page

--- src/components/pages/User/SignInSide.jsx
+++ src/components/pages/User/SignInSide.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 import axios from 'axios'
-import { useHistory } from "react-router-dom";
-import { useCookies } from 'react-cookie';
-import { Link } from "react-router-dom"
+import { useHistory } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://res.cloudinary.com/dhexix4cn/image/upload/v1626617738/teamup/photo-girl1b_nwb8u7.jpg)',
+    backgroundImage:
+      'url(https://res.cloudinary.com/dhexix4cn/image/upload/v1626617738/teamup/photo-girl1b_nwb8u7.jpg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
@@ -40,116 +43,134 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     maxWidth: 75,
     margin: theme.spacing(-3, 12, 10, 0),
-    
   },
-  
-}));
+}))
 
 export default function SignInSide() {
-  const classes = useStyles();
+  const classes = useStyles()
 
-   // use useState hooks
- const [emailLogin, setEmailLogin] = React.useState('')
- const [passwordLogin, setPasswordLogin] = React.useState('')
-  // Npm Cookies  => [ value, setter ] ==> if we want to use setter, we need to set value 
- const [cookie, setCookie] = useCookies(['auth_token']) 
- let [response, setFetchedData] = React.useState('')
- let history = useHistory();
+  // use useState hooks
 
+  const [emailLogin, setEmailLogin] = React.useState('')
+  const [passwordLogin, setPasswordLogin] = React.useState()
+  // Npm Cookies  => [ value, setter ] ==> if we want to use setter, we need to set value
+  const [cookie, setCookie] = useCookies(['auth_token'])
+  let [response, setFetchedData] = React.useState(null)
+  let history = useHistory()
 
- // use api callback
- let fetchData = async () => {
-  response = await axios({
-    method: 'post',
-    url: 'https://teamup-be.herokuapp.com/api/v1/login',
-    data: {
-     email: emailLogin,
-     password: passwordLogin,
-    }, 
-  })
-  console.log(response.data)
-  setCookie('auth_token', response.data.token)
-  setFetchedData(response)
+  // use api callback
+  let fetchData = async () => {
+    try {
+      response = await axios({
+        method: 'post',
+        url: 'https://teamup-be.herokuapp.com/api/v1/login',
+        data: {
+          email: emailLogin,
+          password: passwordLogin,
+        },
+      })
+    } catch (err) {
+      return err
+    }
+    setFetchedData(response)
+    return
+  }
 
-}
+  React.useEffect(() => {
+    fetchData()
+  }, [])
 
+  // submit form function
+  const handleFormSubmission = async (e) => {
+    e.preventDefault()
+    fetchData()
 
- // submit form function
- const handleFormSubmission = async (e) => {
-  e.preventDefault()
-  fetchData()
-  history.push("/dashboard");
-  console.log(
-    `form submitted with values: ${emailLogin}, ${passwordLogin} `
-  )
-}
+    if (!response.data.token) {
+      console.log('authentication failed')
+      return
+    }
+    setCookie('auth_token', response.data.token)
+    history.push('/dashboard')
+  }
 
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container component='main' className={classes.root}>
       <CssBaseline />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-        <img src='https://res.cloudinary.com/dhexix4cn/image/upload/v1626617737/teamup/logo_sbei3p.png' alt='logo' className={classes.logo}/>
-          <Typography 
-           style={{fontWeight:"700"}}
-          variant="h4" align='left'>
+          <img
+            src='https://res.cloudinary.com/dhexix4cn/image/upload/v1626617737/teamup/logo_sbei3p.png'
+            alt='logo'
+            className={classes.logo}
+          />
+          <Typography style={{ fontWeight: '700' }} variant='h4' align='left'>
             Login
           </Typography>
           <Typography
-             style={{fontWeight:"200", fontSize: "18px", marginTop:"10px"}}
-             variant="h6" align='left' >
+            style={{ fontWeight: '200', fontSize: '18px', marginTop: '10px' }}
+            variant='h6'
+            align='left'
+          >
             Welcome back! Please login to your account.
           </Typography>
-          <form className={classes.form} noValidate
-          onSubmit={ e=> {handleFormSubmission(e)}}>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={(e) => {
+              handleFormSubmission(e)
+            }}
+          >
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
               autoFocus
-              onChange={(e) => setEmailLogin(e.target.value)}
+              onChange={(e) => {
+                setEmailLogin(e.target.value)
+              }}
             />
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
               onChange={(e) => setPasswordLogin(e.target.value)}
             />
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               className={classes.submit}
             >
               Sign In
             </Button>
-            <Grid container
-              style={{textAlign:"center"}}>
+            <Grid container style={{ textAlign: 'center' }}>
               <Grid item>
-                 New User? 
-                <Link to="/register"  
-                style={{textDecorationLine:"underline", color:"#7865E5"}}
-                variant="body2">
-                  {" Sign Up!"}
+                New User?
+                <Link
+                  to='/register'
+                  style={{ textDecorationLine: 'underline', color: '#7865E5' }}
+                  variant='body2'
+                >
+                  {' Sign Up!'}
                 </Link>
               </Grid>
             </Grid>
           </form>
         </div>
       </Grid>
-    <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
     </Grid>
-  );
+  )
 }
