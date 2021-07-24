cbaced55fa99d13b0a7ffd8ccf43a4913b2dfd55
xinyingchua <79414040+xinyingchua@users.noreply.github.com>
2021-07-24 19:52:15 +0800

    change resolved links on login, register page

--- src/components/pages/User/SignInSide.jsx
+++ src/components/pages/User/SignInSide.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -10,6 +9,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios'
 import { useHistory } from "react-router-dom";
 import { useCookies } from 'react-cookie';
+import { Link } from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -139,9 +139,9 @@ export default function SignInSide() {
               style={{textAlign:"center"}}>
               <Grid item>
                  New User? 
-                <Link 
+                <Link to="/register"  
                 style={{textDecorationLine:"underline", color:"#7865E5"}}
-                href="#" variant="body2">
+                variant="body2">
                   {" Sign Up!"}
                 </Link>
               </Grid>
