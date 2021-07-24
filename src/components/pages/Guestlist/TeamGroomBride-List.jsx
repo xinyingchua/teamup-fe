d2ca4a9a715d73911c28e2ec15faa398436c3fd4
xinyingchua <79414040+xinyingchua@users.noreply.github.com>
2021-07-25 02:17:27 +0800

    show, get for guestlist done, wip for patch and delete

--- src/components/pages/Guestlist/TeamGroomBride-List.jsx
+++ src/components/pages/Guestlist/TeamGroomBride-List.jsx
@@ -1,79 +1,27 @@
-import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom"
-import axios from 'axios'
 
 
-
-const guestlist = [
-  {
-    name: "Jonathan",
-    status: "attending",
-  },
-  {
-    name: "John",
-    status: "pending",
-  },
-  {
-    name: "Jon",
-    status: "not-attending",
-  },
-  {
-    name: "Jonny",
-    status: "attending",
-  },
-  {
-    name: "John",
-    status: "attending",
-  },
-  
-];
-
-
-const useStyles = makeStyles((theme) => ({
-
-  teamtitle: {
-    display: "flex",
-    flexDirection: "row",
-    paddingTop: "30px",
-    alignItems: "center",
-    justifyContent: "center",
-    
-  },
-
-}));
-
-
-export default function TeamGroom() {
-  const classes = useStyles();
-
+export default function TeamGroomBrideGuestList(props) {
 
   return (
-    
-      <div style={{ margin:'25px'}}>
-      <List className={classes.ulroot}>
-        {guestlist.map((guestlist) => {
-          
 
-          return (
-            <Grid container fullWidth>
-              <Grid item xs={12} md={11} lg={12}>
+              <Grid item key={props} xs={12} md={11} lg={12}>
                   <ListItem
-                    key={guestlist}
+                    key={props.name}
                     dense
                     style={{ paddingBottom:'30px'}}
                   >
                     
                     <ListItemText
                       edge="start"
-                      primary={guestlist.name}
+                      primary={props.name}
                       style={{ display:'flex', alignItems:'center'}}
                     />
                     <Grid
@@ -87,11 +35,11 @@ export default function TeamGroom() {
                     <ListItemSecondaryAction>
                       <IconButton edge="end" aria-label="edit">
                       <Typography style={{fontWeight:'500', color:'#5EAB50', marginRight:'20px'}}>
-                        +5
+                      {props.pax}
                       </Typography>
                       </IconButton>
                       
-                      <Link to="/guest-lists/edit" style={{ textDecoration: "none", color:'#fff' }}>
+                      <Link to={{ pathname: '/guest-lists/edit', state: { _id: props._id} }} style={{ textDecoration: "none", color:'#fff' }}>
                       <Button variant="contained" color="secondary" style={{background:'#60C3F1'}}>
                       Attending
                     </Button>
@@ -99,14 +47,6 @@ export default function TeamGroom() {
                     </ListItemSecondaryAction>
                   </ListItem>
               </Grid>
-            </Grid>
-          );
-        })}
-      </List>
-      </div>
 
-
-
-    
-  );
+  )
 }
